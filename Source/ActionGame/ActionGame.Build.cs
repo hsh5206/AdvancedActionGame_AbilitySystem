7f2b91d995a5a04e966f59396d8f90b4e2fccd16
hsh5206 <hyunhsh5206@gmail.com>
2023-02-12 12:55:46 +0900

    Add AbilitySystemComponent AttributeSetBase GameInstance Set Health&MaxHealth to AttributeSetBase

--- Source/ActionGame/ActionGame.Build.cs
+++ Source/ActionGame/ActionGame.Build.cs
@@ -8,6 +8,10 @@ public class ActionGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+
+		PublicIncludePaths.Add("ActionGame/");
 	}
 }
